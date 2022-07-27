5cc59d6265c7c2a08f7eb8e79d727ef9432fd552
Jorge Mata <juniormata@verizon.net>
2022-07-27 14:38:52 -0700

    Initial Commit

---
+++ CS4540_Project/CS4540_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS4540_ProjectApp.swift
+//  CS4540_Project
+//
+//  Created by Jorge Mata Jr on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CS4540_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS4540_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS4540_Project
+//
+//  Created by Jorge Mata Jr on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
